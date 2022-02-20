ac570f5635c58c497ba8affeffc9b413d4a54020
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-02-20 16:11:19 +0700

    Create Ex1.js

---
+++ BasicJavascript/Ex1/Ex1.js
@@ -0,0 +1,5 @@
+let name = "Codecamp";
+
+console.log(`hello ${1}`);              //hello 1
+console.log(`hello ${"name"}`);         //hello name 
+console.log(`hello ${name}`);           //hello Codecamp
