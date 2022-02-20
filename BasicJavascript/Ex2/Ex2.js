1c5d8ea3ce8704e95418d729e73991208ee195dd
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-02-20 16:32:19 +0700

    Create Ex2.js

---
+++ BasicJavascript/Ex2/Ex2.js
@@ -0,0 +1,6 @@
+let name = "Pond"
+let age = 28
+let address = "88/8 Sukhumvit rd. 39 Klong-Teoi Bangkok"
+let bio = `${name} ${age} ${address}`;
+
+console.log(bio)            //Pond 28 88/8 Sukhumvit rd. 39 Klong-Teoi Bangkok
