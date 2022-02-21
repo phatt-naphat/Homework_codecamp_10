1cc56b511f62b253be3422d33729bcc91c192b92
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-02-21 23:40:32 +0700

    Update Ex8.js

--- BasicJavascript/Condition/Ex8.js
+++ BasicJavascript/Condition/Ex8.js
@@ -1,1 +1,15 @@
-hello
+let score = prompt('Please define your score');
+if (score >= 80){
+  alert("A");
+}
+else if (score < 80 & score >= 70){
+  alert("B");
+}
+else if (score < 70 & score >= 60){
+  alert("C");
+}
+else if (score < 60 & score >= 50){
+  alert("D");
+}
+else { alert("F");
+}
