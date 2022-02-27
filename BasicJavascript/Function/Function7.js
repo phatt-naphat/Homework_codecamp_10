be0303ab8faafca5d0e678ecb8f2245489b42331
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-02-27 16:32:33 +0700

    Create Function7.js

---
+++ BasicJavascript/Function/Function7.js
@@ -0,0 +1,12 @@
+function draw(n){
+    let num = '';
+    let buffer = n**2;
+    for (let i = 1; i <= n; i++){
+        for (let j = 1; j <= n; j++){
+            num += buffer;
+            buffer--;
+        }
+        num += '\n';
+    }
+    console.log(num);
+}
