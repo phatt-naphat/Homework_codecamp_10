37db83e5f43a3d9d986b3e424cf95754176c22ea
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-02-28 14:35:33 +0700

    Create Function21.js

---
+++ BasicJavascript/Function/Function21.js
@@ -0,0 +1,18 @@
+function draw(n){
+    let num = '';
+    let side = 0;
+    for (let i = 0; i < n; i++){
+        for (let j = 1; j <= (2 * n) - 1; j++){
+            if (j == n){
+                num += '*';
+            }
+            else if ( j >= n - side && j <= n + side){
+               num += '*';
+            }
+            else { num += '-'; }
+        }
+        num += '\n';
+        side++;
+    }
+console.log(num)    
+}
