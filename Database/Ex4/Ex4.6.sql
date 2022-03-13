6d3e6a2c3ac341c4bdc75f80e8137ca33553a0ae
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-03-13 17:30:41 +0700

    Update Ex4.6.sql

--- Database/Ex4/Ex4.6.sql
+++ Database/Ex4/Ex4.6.sql
@@ -1,2 +1,2 @@
 SELECT sname from Sailors left join Reserves on Sailors.sid = Reserves.sid 
-left join Boats on Reserves.bid = Boats.bid where Boats.color = 'red' or 'green';
+left join Boats on Reserves.bid = Boats.bid where Boats.color = 'red' or Boats.color = 'green';
