e8f1222529a8a1549615f5069bf9cd0d1ae811b1
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-03-13 16:50:34 +0700

    Update Ex4.3.sql

--- Database/Ex4/Ex4.3.sql
+++ Database/Ex4/Ex4.3.sql
@@ -1,1 +1,1 @@
-SELECT sid from Reserves left join Boats on Reserves.sid = Boats.sid where Color = 'Red';
+SELECT distinct sid from Reserves left join Boats on Reserves.sid = Boats.sid where Color = 'Red';
