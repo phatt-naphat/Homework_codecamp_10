da66d1219c7d30bef7589e0c58942edf097f4120
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-03-21 15:52:02 +0700

    Update Ex4.8.sql

--- Database/Ex4/Ex4.8.sql
+++ Database/Ex4/Ex4.8.sql
@@ -1,1 +1,6 @@
-
+Select distinct sid from Sailors S where S.sid in
+(Select distinct S.sid from Sailors S LEFT JOIN Reserves R 
+ON S.sid = R.sid LEFT JOIN Boats B ON R.bid = B.bid where B.color = 'Red')
+AND S.name NOT IN
+(Select distinct S.sid from Sailors S LEFT JOIN Reserves R
+ON S.sid = R.sid LEFT JOIN Boats B ON R.bid = B.bid where B.color = 'Green');
