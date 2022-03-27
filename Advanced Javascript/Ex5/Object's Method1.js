09e6198ada1c291b1dd58e8387ac215f4166b0d8
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-03-27 15:40:05 +0700

    Update Object's Method1.js

--- Advanced Javascript/Ex5/Object's Method1.js
+++ Advanced Javascript/Ex5/Object's Method1.js
@@ -6,4 +6,13 @@ let user = {
 (user.go)()
 
 
-// alert "John"
+// Error
+
+let user = {
+  name: "John",
+  go: function() { alert(this.name) }
+};
+
+(user.go)()
+
+// Have to add ; at the end of let{} Then it represented alert"John"
