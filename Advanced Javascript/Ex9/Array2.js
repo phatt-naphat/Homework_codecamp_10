6d5e6d85e58e1b49755722b82d12d617055689bf
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-03-29 22:16:53 +0700

    Create Array2.js

---
+++ Advanced Javascript/Ex9/Array2.js
@@ -0,0 +1,15 @@
+//a. 
+let styles = ["Jazz", "Blues"];
+
+//b.
+styles.push("Rock-n-Roll");
+
+//c.
+styles.[Math.floor(styles.length/2)] = "Classics";
+
+//d.
+styles.shift();
+
+//e.
+styles.unshift("Rap", "Raggae");
+
