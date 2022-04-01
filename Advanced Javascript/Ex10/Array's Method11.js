91e6a8b3d581c6ed67289ad2b60f0ecab7ee1e6b
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-04-01 11:41:04 +0700

    Create Array's Method11.js

---
+++ Advanced Javascript/Ex10/Array's Method11.js
@@ -0,0 +1,22 @@
+let array1 = [
+       { name: "apple", birth: "2000-01-01" },
+       { name: "banana", birth: "1990-10-01" },
+       { name: "watermelon", birth: "1985-12-01" },
+     ]
+     array2 [
+       { name: "apple", birth: "2000-01-01", age: 19 },
+       { name: "banana", birth: "1990-10-01", age: 29 },
+       { name: "watermelon", birth: "1985-12-01", age: 33 },
+     ]
+
+
+let array2 = array1.map( function(item) {
+    let birthdate = item.birth.split("-");
+    let obj = {};
+    obj.name = item.name;
+    obj.birth = item.birth;
+    obj.age = 2019 - birthdate[0];
+    return obj;
+})
+
+console.log(obj);
