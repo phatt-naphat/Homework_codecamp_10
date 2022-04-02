8f59f910d36243017a0aebef3d34cac3a32948ec
phatt-naphat <81078081+phatt-naphat@users.noreply.github.com>
2022-04-02 10:20:22 +0700

    Create Array's Method2.5.js

---
+++ Advanced Javascript/Ex10/Array's Method2.5.js
@@ -0,0 +1,9 @@
+let array1 = [
+      { name: "apple", age: 14 },
+      { name: "banana", age: 18 },
+      { name: "watermelon", age: 32 },
+      { name: "pineapple", age: 16 },
+      { name: "peach", age: 24 },
+    ];
+let array2 = array1.filter(x => x.age < 18);
+console.log(array2);
